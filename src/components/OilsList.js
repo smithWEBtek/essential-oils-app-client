5d5c64f77b09bdaed3decae6c5d23c48806b0720
mcclains2003 <mcclains2003@gmail.com>
2017-12-04 08:30:24 -0600

    Created the OilsList component to be more of the index view. The OilCard is being moved more into a show view.

---
+++ src/components/OilsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const OilsList = ({ oils }) => {
+    const renderOils = oils.map(oil =>
+        <ListGroupItem>
+            <Link key={ oil.id } to={`/oils/${oil.id}`}>{ oil.name }</Link>
+        </ListGroupItem>
+    );
+
+    return (
+        <div>
+            <ListGroup>
+                { renderOils }
+            </ListGroup>
+        </div>
+    );
+};
+
+export default OilsList;
