2cd65af93e9059cdc81d2abb0113bb2e5c2de10e
mcclains2003 <mcclains2003@gmail.com>
2017-12-04 08:28:37 -0600

    Added a card theme to the OilCard component.

--- src/components/OilCard.js
+++ src/components/OilCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Card, Button, CardTitle, CardText,
+ CardSubtitle, CardBody } from 'reactstrap';
 
 const OilCard = ({ oil }) => (
-    <div key={ oil.id } className="OilsCard">
-        <h3>Name: { oil.name }</h3>
-        <p>Description: { oil.description }</p>
-        <p>Oil Uses: { oil.uses }</p>
-        <p>Scent Profile: { oil.fragrance_profile }</p>
-        <p>Medical Properties: { oil.medical_properties }</p>
-    </div>
+    <Card key={ oil.id } className="OilsCard">
+        <CardBody>
+            <CardTitle>{ oil.name }</CardTitle>
+            <CardText>{ oil.description }</CardText>
+            <CardSubtitle>Uses:</CardSubtitle>
+            <CardText>{ oil.uses }</CardText>
+            <CardSubtitle>Scent Profile:</CardSubtitle>
+            <CardText>{ oil.fragrance_profile }</CardText>
+            <CardSubtitle>Medical Properties:</CardSubtitle>
+            <CardText>{ oil.medical_properties }</CardText>
+            <Button>Button</Button>
+        </CardBody>
+    </Card>
 );
 
 export default OilCard;
